634b22df395bc669f50c7e7c47251dd6c0ba0f8d
xxc <778994556@qq.com>
2020-08-20 16:35:37 +0800

    update

--- src/main/service/abstract/BaseService.ts
+++ src/main/service/abstract/BaseService.ts
@@ -30,7 +30,7 @@ export default abstract class BaseService<T extends BaseDao, E extends BaseEntit
      * 新增
      * @param entity
      */
-    async add(entity: E) {
+    async add(entity: E | Array<E>) {
         return await this.dao.insert(entity);
     }
 

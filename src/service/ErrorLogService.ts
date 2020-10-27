df2cbf94b2848e6e03ad5833509502df45957489
xxc <778994556@qq.com>
2020-10-27 10:01:42 +0800

    update

--- src/service/ErrorLogService.ts
+++ src/service/ErrorLogService.ts
@@ -1,21 +1,22 @@
 import ErrorLogDao from "../dao/ErrorLogDao";
 import ErrorLog from "../entity/ErrorLog";
 import BaseService from "./abstract/BaseService";
+import BaseResult from "../dto/BaseResult";
 
 export default class ErrorLogService extends BaseService<ErrorLogDao<ErrorLog>, ErrorLog> {
     constructor(context) {
         super(new ErrorLogDao(context));
     }
 
-    async add(response): Promise<string> {
+    async add(response: BaseResult): Promise<string> {
         let errorLog = new ErrorLog();
         errorLog.nick = this.nick;
-        errorLog.api = response.data;
+        errorLog.api = response.api;
         errorLog.message = response.message;
         errorLog.openId = this.openId;
         errorLog.time = this.time().common.base;
         errorLog.params = response.params;
-        errorLog.desc = response;
+        errorLog.desc = response.data;
         return await super.insertOne(errorLog);
     }
 }

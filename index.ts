f5c9265cfe2d5425a9964c743c9bd5161e06a0fc
xxc <778994556@qq.com>
2021-02-04 09:50:42 +0800

    breakChange

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import App from "./base/App";
 // @ts-ignore
 import * as gmspm from "gm-spm";
+import ISpmService from "./src/ISpmService";
 // @ts-ignore
 exports.main = async (context) => {
     const app = new App(context, "main");
@@ -8,10 +9,6 @@ exports.main = async (context) => {
         // do...
     });
 }
-/**
- * 后期检查数据
- * @param context
- */
 // @ts-ignore
 exports.aggregate = async (context) => {
     const app = new App(context, "aggregate");
@@ -21,10 +18,6 @@ exports.aggregate = async (context) => {
     }, need);
 }
 
-/**
- * 后期修改数据
- * @param context
- */
 // @ts-ignore
 exports.update = async (context) => {
     const app = new App(context, "update");
@@ -36,10 +29,6 @@ exports.update = async (context) => {
     }, need);
 }
 
-/**
- * 后期修改数据
- * @param context
- */
 // @ts-ignore
 exports.clean = async (context) => {
     const app = new App(context, "clean");
@@ -53,56 +42,46 @@ exports.clean = async (context) => {
         }
     }, ["tb"]);
 }
-
-//埋点
 // @ts-ignore
 exports.spm = async (context) => {
     return await gmspm.spm.spm(context);
 }
 
-//查询次数统计
 // @ts-ignore
 exports.selectSpm = async (context) => {
     return await gmspm.spm.selectSpm(context);
 }
 
-//查询人数统计
 // @ts-ignore
 exports.selectSpmDis = async (context) => {
     return await gmspm.spm.selectSpmDis(context);
 }
 
-//查询新增人数统计
 // @ts-ignore
 exports.selectSpmDisTotal = async (context) => {
     return await gmspm.spm.selectSpmDisTotal(context);
 }
 
-//查询字段总和统计
 // @ts-ignore
 exports.selectSpmSum = async (context) => {
     return await gmspm.spm.selectSpmSum(context);
 }
 
-//导出统计数据
 // @ts-ignore
 exports.exportStatistics = async (context) => {
     return await gmspm.spm.exportStatistics(context);
 }
 
-//查询奖品数据
 // @ts-ignore
 exports.selectPrize = async (context) => {
     return await gmspm.spm.selectPrize(context);
 }
 
-//导出奖品
 // @ts-ignore
 exports.exportsPrize = async (context) => {
     return await gmspm.spm.exportsPrize(context);
 }
 
-//ID导出
 // @ts-ignore
 exports.exportsNick = async (context) => {
     return await gmspm.spm.exportsNick(context);
@@ -114,233 +93,20 @@ exports.selectBehavior = async (context) => {
     return await gmspm.spm.selectBehavior(context);
 }
 
-const spmFun = {
-    A: "selectSpm",
-    B: "selectSpmDis",
-    C: "selectSpmDisTotal"
+// @ts-ignore
+exports.defaultNickSelect = async (context) => {
+    const app = new App(context, "defaultNickSelect");
+    app.config.globalActivity = false;
+    return await app.run(async function () {
+        await app.getService(ISpmService).defaultNickSelect();
+    });
 }
 
-function generateSpm(config: any[]) {
-
-    function baseSpm(title, type, funName) {
-        return {
-            title: title,
-            type: (() => {
-                if (funName === spmFun.B) {
-                    return type + "Count";
-                } else if (funName === spmFun.C) {
-                    return type + "NewCount";
-                } else {
-                    return type;
-                }
-            })(),
-            parameter: {"type": type},
-            fun: funName
-        }
-    }
-
-    let finalData = [];
-
-    for (let c of config) {
-        if (c.A === true) {
-            finalData.push(baseSpm(c.title, c.type, spmFun.A));
-        } else if (c.B === true) {
-            finalData.push(baseSpm(c.title, c.type, spmFun.B));
-        } else if (c.C === true) {
-            finalData.push(baseSpm(c.title, c.type, spmFun.C));
-        }
-    }
-    return finalData;
-}
-
-/**
- * 获取配置
- * @param context
- */
 // @ts-ignore
 exports.selectUiTitleAndType = async (context) => {
     const app = new App(context, "selectUiTitleAndType");
     app.config.globalActivity = false;
     return await app.run(async function () {
-        /**
-         * PrizeConfig
-         * 奖品查询和展示字段
-         */
-        let prizeConfig = [  //奖品展示标题
-            {
-                title: "ID", type: "nick", targetField: "nick"
-            },
-            {
-                title: "奖品名", type: "prizeName", targetField: "prizeName"
-            },
-            {
-                title: "获得时间", type: "time", targetField: "time"
-            },
-            {
-                title: "姓名", type: "name", targetField: "ext.name"
-            },
-            {
-                title: "电话", type: "tel", targetField: "ext.tel"
-            },
-            {
-                title: "省", type: "province", targetField: "ext.province"
-            },
-            {
-                title: "市", type: "city", targetField: "ext.city"
-            },
-            {
-                title: "区", type: "district", targetField: "ext.district"
-            },
-            {
-                title: "详细地址", type: "desc", targetField: "ext.desc"
-            }
-        ]
-        /**
-         * prizeOptions
-         * 奖品类型选项
-         */
-        let prizeOptions = [
-            {
-                "title": "抽奖",
-                "value": "lottery"
-            },
-            {
-                "title": "排行榜",
-                "value": "rank"
-            }
-        ]
-        /**
-         * 所有配置
-         */
-        app.response.data = {
-            //统计数据配置
-            "statisticsTitleAndTypeArr": [
-                {
-                    "title": "PV",
-                    "type": "PV",
-                    "parameter": {"type": "view"},
-                    "fun": "selectSpm"
-                },
-                {
-                    "title": "UV",
-                    "type": "UV",
-                    "parameter": {"type": "view"},
-                    "fun": "selectSpmDis"
-                },
-                {
-                    "title": "newUV",
-                    "type": "newUV",
-                    "parameter": {"type": "view"},
-                    "fun": "selectSpmDisTotal"
-                },
-                ...generateSpm([
-                    {title: "", type: "", A: false, B: false, C: false},
-                ]),
-            ],
-            //统计数据导出配置
-            "exportStatistics": {
-                "fixParameter": {},//固定参数，调用接口会默认传入内部所有参数
-                "fun": "exportStatistics",//接口名称
-                "title": "导出"
-            },
-            //中奖数据查询配置
-            "selectWinnerTitleAndTypeArr": {
-                "title": "中奖数据查询", //标题
-                "showTime": true,//是否需要时间查询
-                "fun": "selectPrize",//云函数方法名，自定义
-                "fixParameter": {
-                    // sort: {},
-                    // filter: {},
-                    winnerTitleAndTypeArr: prizeConfig
-                },//固定参数，查询接口时候会默认带上内部所有参数
-                "parameter": {  //动态参数，比如 type:'type值1'
-                    "type": {
-                        "type": "radio", //单选框
-                        "title": "类型标题",
-                        "options": [
-                            ...prizeOptions
-                        ]
-                    }
-                },
-                "data": prizeConfig
-            },
-            //中奖数据导出
-            "winnerTitleAndTypeArr": [
-                {
-                    "title": "中奖数据",
-                    "export": {
-                        "title": "类型", //标题
-                        "showTime": true,//是否需要时间查询
-                        "fun": "exportsPrize",//云函数方法名，自定义
-                        "fixParameter": {
-                            sort: {
-                                //排序
-                                time: -1
-                            },
-                            // filter: {
-                            //     //额外查询
-                            // },
-                            winnerTitleAndTypeArr: prizeConfig
-                        },//固定参数，查询接口时候会默认带上内部所有参数
-                        "parameter": {  //动态参数，比如 type:'type值1'
-                            "type": {
-                                "type": "radio", //单选框
-                                "title": "类型标题",
-                                "options": [
-                                    ...prizeOptions
-                                ]
-                            }
-                        }
-                    }
-                }
-            ],
-            //用户昵称导出
-            "userNicksExportsArr": [
-                {
-                    "title": "标题",
-                    "export": {
-                        "title": "标题", //标题
-                        "showTime": true,//是否需要时间查询
-                        "fun": "exportsNick",//云函数方法名，自定义
-                        "fixParameter": {
-                            // filter: {
-                            //     //额外查询
-                            // }
-                        },//固定参数，查询接口时候会默认带上内部所有参数
-                        "parameter": {  //动态参数，比如 type:'type值1'
-                            "type": {
-                                "type": "radio", //单选框
-                                "title": "类型标题",
-                                "options": []
-                            }
-                        }
-                    }
-                }
-            ],
-            //用户ID查询
-            "behaviorTitleAndTypeArr": [
-                {
-                    "title": "行为数据",
-                    "export": {
-                        "title": "类型", //标题
-                        "showTime": true,//是否需要时间查询
-                        "fun": "selectBehavior",//云函数方法名，自定义
-                        "fixParameter": {},//固定参数，查询接口时候会默认带上内部所有参数
-                        "parameter": {  //动态参数，比如 type:'type值1'
-                            "type": {
-                                "type": "radio", //单选框
-                                "title": "类型标题",
-                                "options": [
-                                    {
-                                        "title": "邀请明细",
-                                        "value": "assistAll"
-                                    }
-                                ]
-                            }
-                        }
-                    }
-                }
-            ]
-        }
+        app.response.data = app.getService(ISpmService).baseData;
     });
 };
---
+++ src/ISpmService.ts
@@ -0,0 +1,352 @@
+import BaseService from "../base/service/abstract/BaseService";
+import SpmDao from "../base/dao/SpmDao";
+import Spm from "../base/entity/Spm";
+import App from "../base/App";
+import Utils from "../base/utils/Utils";
+
+
+export default class ISpmService extends BaseService<SpmDao<Spm>, Spm> {
+    constructor(app: App) {
+        super(SpmDao, app);
+    }
+
+    get baseData() {
+        this.spmConfig = [
+            {title: "", type: "", A: false, B: true, C: false}
+        ]
+        this.prizeConfig = [
+            {title: "ID", type: "nick", targetField: "nick"},
+            {title: "奖品名", type: "prizeName", targetField: "prizeName"},
+            {title: "获得时间", type: "time", targetField: "time"},
+            {title: "姓名", type: "name", targetField: "ext.name"},
+            {title: "电话", type: "tel", targetField: "ext.tel"},
+            {title: "省", type: "province", targetField: "ext.province"},
+            {title: "市", type: "city", targetField: "ext.city"},
+            {title: "区", type: "district", targetField: "ext.district"},
+            {title: "详细地址", type: "desc", targetField: "ext.desc"}
+        ]
+        this.prizeOptions = [
+            this.generateOptions("排行榜", "rank"),
+            this.generateOptions("抽奖", "lottery"),
+        ]
+        // this.prizeExtFilter = {};
+        // this.prizeSort = {};
+        this.addWinnerExport("中奖数据", {});
+        this.addDefaultUserNickSelect();
+        this.addDefaultUserNickExport()
+
+        return {
+            statisticsTitleAndTypeArr: this.spmConfig,
+            "exportStatistics": {
+                "fixParameter": {},//固定参数，调用接口会默认传入内部所有参数
+                "fun": "exportStatistics",//接口名称
+                "title": "导出"
+            },
+            "selectWinnerTitleAndTypeArr": {
+                "title": "中奖数据查询", //标题
+                "showTime": true,//是否需要时间查询
+                "fun": "selectPrize",//云函数方法名，自定义
+                "fixParameter": {
+                    // sort: {},
+                    // filter: {},
+                    winnerTitleAndTypeArr: this.prizeConfig,
+                    ...this.prizeParameter
+                },//固定参数，查询接口时候会默认带上内部所有参数
+                "parameter": {  //动态参数，比如 type:'type值1'
+                    "type": {
+                        "type": "radio", //单选框
+                        "title": "类型标题",
+                        "options": [
+                            ...this.prizeOptions
+                        ]
+                    }
+                },
+                "data": this.prizeConfig
+            },
+            "winnerTitleAndTypeArr": this.exportWinnerConfig,
+            "userNicksExportsArr": this.userNickExportConfig,
+            "behaviorTitleAndTypeArr": this.userNickSelectConfig
+        }
+    }
+
+    spmFun = {
+        A: "selectSpm",
+        B: "selectSpmDis",
+        C: "selectSpmDisTotal"
+    };
+
+    _: other = {
+        prizeOptions: [],
+        prizeConfig: [],
+        spmConfig: [
+            {
+                "title": "PV",
+                "type": "PV",
+                "parameter": {"type": "view"},
+                "fun": this.spmFun.A
+            },
+            {
+                "title": "UV",
+                "type": "UV",
+                "parameter": {"type": "view"},
+                "fun": this.spmFun.B
+            },
+            {
+                "title": "newUV",
+                "type": "newUV",
+                "parameter": {"type": "view"},
+                "fun": this.spmFun.C
+            }
+        ],
+        prizeFixParameter: {},
+        exportWinnerConfig: [],
+        userNicksExportConfig: [],
+        userNickSelectConfig: []
+    }
+
+    get spmConfig() {
+        return this._.spmConfig;
+    }
+
+    get prizeOptions() {
+        return this._.prizeOptions;
+    }
+
+    get prizeConfig() {
+        return this._.prizeConfig;
+    }
+
+    get prizeParameter() {
+        return this._.prizeFixParameter;
+    }
+
+    get exportWinnerConfig() {
+        return this._.exportWinnerConfig;
+    }
+
+    get userNickSelectConfig() {
+        return this._.userNickSelectConfig;
+    }
+
+    get userNickExportConfig() {
+        return this._.userNicksExportConfig;
+    }
+
+    set prizeParameter(v) {
+        this._.prizeFixParameter = v;
+    }
+
+    set prizeOptions(v) {
+        this._.prizeOptions = v;
+    }
+
+    set prizeConfig(v) {
+        this._.prizeConfig = v;
+    }
+
+    set prizeSort(v) {
+        this._.prizeFixParameter.sort = v;
+    }
+
+    set prizeExtFilter(v) {
+        this._.prizeFixParameter.filter = v;
+    }
+
+    set spmConfig(config: any[]) {
+        this._.spmConfig.push(...this.generateSpm(config));
+    }
+
+    set exportWinnerConfig(v) {
+        this._.exportWinnerConfig.push(v);
+    }
+
+    set userNickExportConfig(v) {
+        this._.userNicksExportConfig.push(v);
+    }
+
+    set userNickSelectConfig(v) {
+        this._.userNickSelectConfig.push(v);
+    }
+
+    generateSpm(config: any[]) {
+        let {spmFun} = this;
+
+        function baseSpm(title, type, funName) {
+            return {
+                title: title,
+                type: (() => {
+                    if (funName === spmFun.B) {
+                        return type + "Count";
+                    } else if (funName === spmFun.C) {
+                        return type + "NewCount";
+                    } else {
+                        return type;
+                    }
+                })(),
+                parameter: {"type": type},
+                fun: funName
+            }
+        }
+
+        let finalData = [];
+        for (let c of config) {
+            if (c.A === true) {
+                finalData.push(baseSpm(c.title, c.type, spmFun.A));
+            } else if (c.B === true) {
+                finalData.push(baseSpm(c.title, c.type, spmFun.B));
+            } else if (c.C === true) {
+                finalData.push(baseSpm(c.title, c.type, spmFun.C));
+            }
+        }
+        return finalData;
+    }
+
+    generateOptions(title, value) {
+        return {
+            title,
+            value
+        }
+    }
+
+    addWinnerExport(title, {
+        prizeConfig = this.prizeConfig,
+        prizeOptions = this.prizeOptions,
+        parameter = this.prizeParameter,
+        fun = ""
+    }) {
+        let p = {
+            "title": title,
+            "export": {
+                "title": "类型", //标题
+                "showTime": true,//是否需要时间查询
+                "fun": fun || "exportsPrize",//云函数方法名，自定义
+                "fixParameter": {
+                    winnerTitleAndTypeArr: prizeConfig,
+                    ...parameter
+                },//固定参数，查询接口时候会默认带上内部所有参数
+                "parameter": {  //动态参数，比如 type:'type值1'
+                    "type": {
+                        "type": "radio", //单选框
+                        "title": "类型标题",
+                        "options": [
+                            ...prizeOptions
+                        ]
+                    }
+                }
+            }
+        }
+        this.exportWinnerConfig = p;
+        return p;
+    }
+
+    addUserNickExport(title, {
+        parameter = {},
+        options = [],
+        fun = ""
+    }) {
+        let e = {
+            "title": title,
+            "export": {
+                "title": title, //标题
+                "showTime": true,//是否需要时间查询
+                "fun": fun || "exportsNick",//云函数方法名，自定义
+                "fixParameter": {
+                    // filter: {
+                    //     //额外查询
+                    // }
+                    ...parameter
+                },//固定参数，查询接口时候会默认带上内部所有参数
+                "parameter": {  //动态参数，比如 type:'type值1'
+                    "type": {
+                        "type": "radio", //单选框
+                        "title": "类型标题",
+                        "options": [
+                            ...options
+                        ]
+                    }
+                }
+            }
+        }
+        this.userNickExportConfig = e;
+        return e;
+    }
+
+    addUserNickSelect(title, {
+        parameter = {},
+        options = [],
+        fun = ""
+    }) {
+        let u = {
+            "title": title,
+            "export": {
+                "title": title, //标题
+                "showTime": true,//是否需要时间查询
+                "fun": fun || "selectBehavior",//云函数方法名，自定义
+                "fixParameter": parameter,//固定参数，查询接口时候会默认带上内部所有参数
+                "parameter": {  //动态参数，比如 type:'type值1'
+                    "type": {
+                        "type": "radio", //单选框
+                        "title": "类型标题",
+                        "options": [
+                            ...options
+                        ]
+                    }
+                }
+            }
+        }
+
+        this.userNickSelectConfig = u;
+        return u;
+    }
+
+    addDefaultUserNickSelect() {
+        let options = this.spmConfig.map(v => this.generateOptions(v.title, v.parameter.type));
+        return this.addUserNickSelect("行为数据", {
+            options,
+            fun: "defaultNickSelect"
+        });
+    }
+
+    addDefaultUserNickExport() {
+        let options = this.spmConfig.map(v => this.generateOptions(v.title, v.parameter.type));
+        return this.addUserNickExport("行为数据", {
+            options
+        });
+    }
+
+    async defaultNickSelect() {
+        let {activityId, type, nick, startTime, endTime, page, size} = this.data;
+        let title: string = this.baseData.statisticsTitleAndTypeArr.find(v1 => v1.parameter.type === type)?.title?.replace(/(次数)|(人数)/g, "");
+        let filter = {
+            activityId,
+            type,
+            nick,
+            time: {
+                $gte: startTime,
+                $lte: endTime
+            }
+        }
+        Utils.cleanObj(filter);
+        page = page || 1;
+        size = size || 30;
+        let rs = await this.pageList(filter, {
+            page,
+            size,
+            project: {
+                _id: 0,
+                nick: 1,
+                time: 1
+            }
+        })
+        this.response.data.behaviorList = [
+            {
+                "behaviorInformationArr": [
+                    ...rs.data.map(v => `【${v.nick || "未授权用户"}】在【${v.time}】触发【${title}】`)
+                ],
+                "title": `【${title}】共${rs.total}条`,
+                "type": type
+            }
+        ]
+    }
+
+}

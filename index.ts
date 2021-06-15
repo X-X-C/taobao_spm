04eadc593155971e59ec2489d11fd77afeae672d
xxc <778994556@qq.com>
2021-06-15 17:59:17 +0800

    udpate

--- index.ts
+++ index.ts
@@ -44,18 +44,5 @@ export async function selectBehavior(context) {
     return await gmspm.spm.selectBehavior(context);
 }
 
-const modules = [SpmService];
-for (let entry of Object.entries(XApp.exports)) {
-    // @ts-ignore
-    exports[entry[0]] = async (context) => {
-        const app = new App(context, entry[0]);
-        app.runNeedParams = entry[1].params || {};
-        if (!entry[1].needGlobalParam) {
-            app.globalNeedParams = {};
-        }
-        entry[1].before.forEach(v => v.call(app.before))
-        return await app.run(async function () {
-            await app.getService(entry[1].constructor)[entry[0]]();
-        });
-    }
-}
+[SpmService];
+exports = XApp.initExpose(App);
